261cc01d9380f306b21b948b3c877ba16384cd1b
navyadeekonda5-arch <navyadeekonda5@gmail.com>
2026-05-04 22:35:27 +0530

    Function Expression - Count Vowels in a String

---
+++ vowels.js
@@ -0,0 +1,13 @@
+const countVowels = function(str) {
+    let count = 0;
+    const vowels = "aeiouAEIOU";
+    
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    
+    return count;
+};
+console.log(countVowels("Hello, World!"));
